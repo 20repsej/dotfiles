c9cf9de5140c8cd71354e526bba7f591a6d66850
Jesper Berg <jesper.berg@mailbox.org>
2021-10-05 23:36:35 +0200

    updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar --config=$HOME/.config/polybar/colorfulconfig DP >>/tmp/polybar1.log 2>&1 & disown
 polybar --config=$HOME/.config/polybar/colorfulconfig HDMI >>/tmp/polybar2.log 2>&1 & disown
+polybar --config=$HOME/.config/polybar/colorfulconfig HDMILEFT >>/tmp/polybar3.log 2>&1 & disown
+polybar --config=$HOME/.config/polybar/colorfulconfig HDMIRIGHT >>/tmp/polybar4.log 2>&1 & disown
 
 echo "Bars launched..."
